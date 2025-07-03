db8cabe54fe249f2379a3e2ef1e3cee7527cc268
novek777 <47804491+KevonHernandez@users.noreply.github.com>
2025-07-03 11:21:34 -0600

    Update urls.py

--- ProyectoSeguro/AdmiSer/urls.py
+++ ProyectoSeguro/AdmiSer/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('logout/', logout, name='logout'),
     path('servicio/<str:accion>/<int:servicio_id>/', accion_servicio, name='accion_servicio'),
     path('servidores/', listar_servidores, name='listar_servidores'),
-
-
+    path('validar-password/', validar_password, name='validar_password'),
+    path('verificar_estados/', verificar_estados_servicios, name='verificar_estados_servicios'),
 ]
 
 
