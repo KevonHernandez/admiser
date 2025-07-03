5f7745296ee8f09cf508329eaef81ca1ded100bd
novek777 <47804491+KevonHernandez@users.noreply.github.com>
2025-07-03 11:20:28 -0600

    Update utilsotp.py

--- ProyectoSeguro/AdmiSer/views/utilsotp.py
+++ ProyectoSeguro/AdmiSer/views/utilsotp.py
@@ -6,11 +6,23 @@ from ..models import OTPToken
 
 
 def generar_codigo_otp(longitud=6):
-    """Genera un OTP numérico."""
+    """
+    Genera un OTP numérico.
+    Args:
+        longitud (int): Longitud del código OTP a generar. Por defecto es 6.    
+    Returns:
+        str: Un código OTP numérico de la longitud especificada.
+    """
     return ''.join(str(random.randint(0, 9)) for _ in range(longitud))
 
 def crear_otp_unico(usuario):
-    """Borra cualquier OTP previo del usuario y genera uno nuevo."""
+    """
+    Borra cualquier OTP previo del usuario y genera uno nuevo.
+    Args:
+        usuario (Usuario): El objeto Usuario para el cual se generará el OTP.
+    Returns:
+        tuple: Un código OTP y un ID de sesión único.
+    """
     OTPToken.objects.filter(user=usuario).delete()
 
     codigo = generar_codigo_otp()
